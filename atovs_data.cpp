c0f6a93b2d2c96defc08807767a58c198612e57c
dex252 <dex252@bk.ru>
2019-04-04 20:39:26 +1000

    Some change in atovs class, cp files 50%

--- atovs_data.cpp
+++ atovs_data.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include "gzip_reader.cpp"
 #include "class_track.cpp"
+#include <dirent.h>
 
 using namespace std;
 
@@ -14,25 +15,129 @@ class AtovsData
 {
 private:
   string atovsPath;
-  string tempAtovs;
   string tempDate;
-  string date;
 
 public:
   AtovsData(Track track, string iAtovsPath, string temp)
   {
     atovsPath = iAtovsPath;
-    tempAtovs = temp + "atovs/";
-    tempDate = temp + "date/";
+    tempDate = temp + "data/";
 
-    checkDate(track);
+    downloadL1B(track);
   }
 
-  void downloadL1B()
+  void downloadL1B(Track track)
   {
+    list<string> date;
+    string dateInTrack("first date");
+
+    for (Vector n : track.trackWay)
+    {
+      if (n.yymmdd != dateInTrack)
+      {
+        dateInTrack = n.yymmdd;
+        date.push_back(dateInTrack);
+      }
+    }
+
+    for (string fullPathToAtovs : date)
+    {
+      fullPathToAtovs = atovsPath + fullPathToAtovs;
+      cout << "ALL ==  " << fullPathToAtovs << endl;
+     // fullPathToAtovs = "https://irods4.satellite.dvo.ru:1247/irods4.satellite.dvo.ru/services/StorageManagementIRods?res=irods_storage#/home/slava/Projects/selection_l1b_format_noaa/TEMP/atovs";
+    // https://beda.botik.ru:8080/beda.botik.ru/services/StorageManagementIRods?res=irods_storage#
+      //ils ${IATOVSPATH}/${BEGIN_DATE_DIR} >${FILELIST}   str.c_str();
+      copyAllFiles(fullPathToAtovs, tempDate);
+    }
   }
 
-  void checkDate(Track track)
+  int copyAllFiles(std::string sorc, std::string dest)
+  {
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(sorc.c_str())) != NULL)
+    {
+      while ((ent = readdir(dir)) != NULL)
+      { /* print all the files and directories within directory */
+        if (ent->d_name != std::string("."))
+        { //REMOVE PWD
+          if (ent->d_name != std::string(".."))
+          { //REMOVE PARENT DIR
+
+            std::string copy_sorc = sorc + "/" + ent->d_name;
+            std::string copy_dest = dest + "/" + ent->d_name;
+
+            std::cout << "cp " << copy_sorc << " -> " << copy_dest << std::endl;
+
+            copyFile(copy_sorc.c_str(), copy_dest.c_str());
+          }
+        }
+      }
+
+      std::cout << std::endl;
+
+      closedir(dir);
+    }
+    else
+    {
+      /* could not open directory */
+      perror("");
+      return EXIT_FAILURE;
+    }
+
+    return -3;
+  }
+
+  void copyFile(const char *fileNameFrom, const char *fileNameTo)
+  {
+
+    char buff[BUFSIZ];
+    FILE *in, *out;
+    size_t n;
+
+    in = fopen(fileNameFrom, "rb");
+    out = fopen(fileNameTo, "wb");
+    while ((n = fread(buff, 1, BUFSIZ, in)) != 0)
+    {
+      fwrite(buff, 1, n, out);
+    }
+  }
+
+  int dir(std::string path)
+  {
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(path.c_str())) != NULL)
+    {
+
+      while ((ent = readdir(dir)) != NULL)
+      { /* print all the files and directories within directory */
+        if (ent->d_name != std::string("."))
+        { //REMOVE PWD
+          if (ent->d_name != std::string(".."))
+          { //REMOVE PARENT DIR
+
+            std::cout << path << "/" << ent->d_name << std::endl;
+          }
+        }
+      }
+
+      std::cout << std::endl;
+
+      closedir(dir);
+    }
+    else
+    {
+      /* could not open directory */
+      perror("");
+      return EXIT_FAILURE;
+    }
+
+    return -2;
+  }
+  /* void checkDate(Track track)
   {
     //data = track.startDate.substr(0, 4);
     string dateFile = tempDate + "dates";
@@ -48,7 +153,8 @@ public:
     }
 
     out.close();
-  }
+  } */
+
   void decompressTry(string input, string output)
   {
     GzipReader GzipReader;
